54ef370046c19153c644b88acf71ffb0738559a2
erdem yigitoglu <erdemyigitoglu@gmail.com>
2023-11-25 00:42:05 +0300

    added minus function

--- calc.js
+++ calc.js
@@ -4,4 +4,9 @@ function add(num1, num2) {
 	}
 	return num1 + num2
 }
-add(5, 4);
+function minus(num1, num2) {
+	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+		return "ERROR"
+	}
+	return num1 - num2
+}
