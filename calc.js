bc4a136f83c7845deb41fa612fa24b24936a130e
erdem yigitoglu <erdemyigitoglu@gmail.com>
2023-11-25 00:43:45 +0300

    added multiply function

--- calc.js
+++ calc.js
@@ -10,3 +10,9 @@ function minus(num1, num2) {
 	}
 	return num1 - num2
 }
+function multiply(num1, num2) {
+	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+		return "ERROR"
+	}
+	return num1 * num2
+}
