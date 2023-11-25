df47957d3f49474ab86366092beaee199cef29c6
erdem yigitoglu <erdemyigitoglu@gmail.com>
2023-11-25 21:05:14 +0300

    added operate(() function

--- calc.js
+++ calc.js
@@ -25,3 +25,14 @@ function divide(num1, num2) {
 let num1 = 0
 let num2 = 0;
 let operator = "";
+let result = 0;
+function operate(num1, operator, num2) {
+	if (operator === "+") {
+		return add(num1, num2)
+	} else if (operater === "-") {
+		return subtract(num1, num2)
+	} else if (operator === "*") {
+		return multiply(num1, num2) 
+	} else if {operator === "/") {
+		num2 !== 0 ? return divide(num1, num2) : return "divition error"
+	}
