30ca06ceac5ebece9e305c7ddc8f9016dfd96e45
erdem yigitoglu <erdemyigitoglu@gmail.com>
2023-11-25 21:08:33 +0300

    deleted type check in operation functions I plan to convert string numbers to numbers before operation functions are called. operetors wont be added to numbers. So I don't need to check if a variable is number after that.

--- calc.js
+++ calc.js
@@ -1,29 +1,17 @@
 function add(num1, num2) {
-	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
-		return "ERROR"
-	}
 	return num1 + num2
 }
 function subtract(num1, num2) {
-	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
-		return "ERROR"
-	}
 	return num1 - num2
 }
 function multiply(num1, num2) {
-	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
-		return "ERROR"
-	}
 	return num1 * num2
 }
 function divide(num1, num2) {
-	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
-		return "ERROR"
-	}
 	return num1 / num2
 }
-let num1 = 0
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
 let result = 0;
 function operate(num1, operator, num2) {
