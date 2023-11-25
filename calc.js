29eb3164066e91ec5395bb42a91482a16a1f9bfb
erdem yigitoglu <erdemyigitoglu@gmail.com>
2023-11-25 17:42:36 +0300

    added divide() function

--- calc.js
+++ calc.js
@@ -16,3 +16,9 @@ function multiply(num1, num2) {
 	}
 	return num1 * num2
 }
+function divide(num1, num2) {
+	if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+		return "ERROR"
+	}
+	return num1 / num2
+}
